648e6ea92a88ef41a972594aaaba4ac86e51f98a
Mohamed Elakrouche <elakrouche@gmail.com>
2024-10-16 12:47:04 +0200

    First challenge PHP

---
+++ challenge1.php
@@ -0,0 +1,9 @@
+<?php 
+$nameBook="Voyage avec un âne dans les Cévennes";
+$read=false;
+$yearOfRelease=2017;
+$priceBook=6.20;
+
+echo($nameBook."</br>".($read ? "lu" : "non lu")."<br>".$yearOfRelease."<br>".$priceBook);
+
+?>
