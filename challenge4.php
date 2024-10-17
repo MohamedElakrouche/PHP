f423ff32c56829add8dabcbd604d33cae1da3b59
Mohamed Elakrouche <elakrouche@gmail.com>
2024-10-17 07:55:24 +0200

    Ajout des 4 premiers challenges PHP

---
+++ challenge4.php
@@ -0,0 +1,28 @@
+<?php
+
+// Création du tableau multidimensionnel des films et des acteurs
+
+$movies = 
+        [
+            [
+            "title" => "The Black Arrow",
+            "actors" => ["Louis Hayward", "Janet Blair", "George Macready"]
+            ],
+             [
+        "title" => "The Master of Ballantrae",
+        "actors" => ["Errol Flynn", "Roger Livesey", "Anthony Steel"]
+            ],
+            [
+        "title" => "Dr. Jekyll and Mr. Hyde (1941)",
+        "actors" => ["Spencer Tracy", "Ingrid Bergman", "Lana Turner"]
+            ]
+        ];
+
+// Boucle pour parcourir et afficher les films et leurs acteurs
+for ($i = 0; $i < count($movies); $i++) 
+    {
+    echo "Dans le film " . $movies[$i]['title'] . ", les principaux acteurs sont : ";
+    echo implode(", ", $movies[$i]['actors']) . ".<br>";
+    }
+
+?>
