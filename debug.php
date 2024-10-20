88b48f3aee3e8818610d4ba7b868a2ac532e9376
Mohamed Elakrouche <elakrouche@gmail.com>
2024-10-20 19:04:45 +0200

    Challenge 7

---
+++ debug.php
@@ -0,0 +1,28 @@
+<?php
+
+// Tableau des survivants
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Mohamed El akrouche" => [
+        "city" => "Gagny",
+        "weapon"=>"sabre"
+    ]
+]; 
+
+// We write code here
+
+var_dump($characters);
+
+?>
